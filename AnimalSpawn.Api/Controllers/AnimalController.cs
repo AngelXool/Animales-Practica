11ddd327f921cdb77f682b3265941405ffdf921a
AngelXool <66449797+AngelXool@users.noreply.github.com>
2020-10-06 13:10:50 -0500

    first commit

---
+++ AnimalSpawn.Api/Controllers/AnimalController.cs
@@ -0,0 +1,33 @@
+﻿using AnimalSpawn.Domain.Interfaces;
+using AnimalSpawn.Infraestruture.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+
+namespace AnimalSpawn.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        private readonly IAnimalRepository _repository;
+        public AnimalController(IAnimalRepository repository)
+        {
+            this._repository = repository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var animals = await _repository.GetAnimals();
+            return Ok(animals);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var animal = await _repository.GetAnimal(id);
+            return Ok(animal);
+        }
+        ///Siguiente clase 25 de Septiembre
+    }
+}
---
+++ AnimalSpawn.Infraestruture/Repositories/AnimalRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnimalSpawn.Domain.Entities;
+using AnimalSpawn.Domain.Interfaces;
+using AnimalSpawn.Infraestruture.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalSpawn.Infraestruture.Repositories
+{
+    public class AnimalRepository : IAnimalRepository
+    {
+
+        private readonly AnimalSpawnContext _context;
+
+        public AnimalRepository(AnimalSpawnContext context)
+        {
+            this._context = context;
+
+        }
+
+        public async Task<Animal> GetAnimal(int id)
+        {
+            var animal = await _context.Animal.FirstOrDefaultAsync(animal=>animal.Id==id);
+            return animal;
+        }
+
+        public async Task<IEnumerable<Animal>> GetAnimals()
+        {
+            var animals = await _context.Animal.ToListAsync();
+            return animals;
+        }
+    }
+}
